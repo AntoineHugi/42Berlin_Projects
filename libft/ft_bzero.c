e7ac94c8bffb46b7bbf07808dcba1108ecfba241
AntoineHugi <antoine.hugi@gmail.com>
2024-11-13 12:32:49 +0100

    fixed a few ex synthax, changed putnbr

--- libft/ft_bzero.c
+++ libft/ft_bzero.c
@@ -17,7 +17,7 @@ void	ft_bzero(void *s, size_t n)
 	unsigned char	*ptr;
 	size_t			i;
 
-	if (s == 0)
+	if (!s)
 	{
 		return ;
 	}
--- libft/ft_strncmp.c
+++ libft/ft_strncmp.c
@@ -17,7 +17,7 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	size_t	i;
 
 	i = 0;
-	while ((*s1 != 0 && *s2 != 0) && i < n)
+	while (*s1 && *s2 && i < n)
 	{
 		if (*s1 != *s2)
 		{
