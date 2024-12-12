6d9939f9792635b245bd5f5b136f1b8391c3378a
Antoine Hugi 42 <ahugi@student.42berlin.de>
2024-12-12 17:39:37 +0100

    Updates to the monitoring script

--- Born2BeRoot/monitoring.sh
+++ Born2BeRoot/monitoring.sh
@@ -1,41 +1,41 @@
 #!/bin/bash
 
-# Display the architecture of the operating system and kernel version
+#info on the architecture of the operating system and kernel version
 arch_info=$(uname -a)
 
-# Display the number of physical processors
-physical_processors=$(grep -c '^processor' /proc/cpuinfo)
+#number of physical processors (-c returns the count)
+physical_processors=$(grep -c "processor" /proc/cpuinfo)
 
-# Display the number of virtual processors
+#the number of virtual processors
 virtual_processors=$(nproc --all)
 
-# Display the available RAM and utilization rate
-ram_info=$(free -m | awk '{print $7, $3*100/$2 }')
+#available RAM and utilization rate
+ram_info=$(free -m | grep "Mem" | awk '{print "available: "$7, ", percentage used: " $3/$2*100 "%"}')
 
-# Display the available storage and utilization rate
-storage_info=$(df -h --total | awk '{print $1, $3, $2, $5}')
+#available storage and utilization rate
+storage_info=$(df -h --total | grep "total" | awk '{print "available: " $4 ", percentage used: " $5 "%"}')
 
-# Display the CPU utilization rate, need to simplify code.
-cpu_utilisation=$(top -bn1 | grep "Cpu(s)" | sed "s/.*, *\([0-9.]*\)%* id.*/\1/" | awk '{printf "CPU Utilization: %.2f%%", 100 - $1}')
+#CPU utilization rate.
+cpu_utilisation=$(top -bn1 | grep "Cpu(s)" | awk '{print ($2 + $3) "%"}')
 
-# Display the date and time of the last reboot
-last_reboot=$(who -b | awk '{print $3, $4, $5}')
+#date and time of the last reboot
+last_reboot=$(who -b | awk '{print $3, $4}')
 
-# Check if LVM is active
+#check if LVM is active
 lvm_status=$(lsblk | grep -q lvm && echo "LVM is active" || echo "LVM is not active")
 
-# Display the number of active connections
-active_connections=$(netstat -tun | grep -c ESTABLISHED)
+#number of active connections
+active_connections=$(netstat -tn | grep -c ESTABLISHED)
 
 # Display the number of users using the server
-user_count=$(who | wc -l)
+user_count=$(who | wc -w)
 
 # Display the IPv4 address and MAC address
-ipv4_info=$(ip a | awk '/inet / && !/127.0.0.1/ {print "IPv4 Address: "$2}')
-mac_info=$(ip a | awk '/ether/ {print "MAC Address: "$2}')
+ipv4=$(ip a | awk '/inet / && !/127.0.0.1/ {print $2}')
+mac=$(ip link | awk '/ether/ {print $2}')
 
 # Display the number of commands executed with sudo
-sudo_command_count=$(grep -c "COMMAND" /var/log/sudo.log 2>/dev/null || echo "0")
+sudo=$(grep -c "COMMAND" /var/log/sudo/sudo.log)
 
 
 wall "  Architecture and Kernel Version: $arch_info
@@ -44,10 +44,10 @@ wall "  Architecture and Kernel Version: $arch_info
         RAM info: $ram_info
         Storage info: $storage_info
         CPU utilisation: $cpu_utilisation
-        Last Reboot: $last_reboot
+        Last Reboot: $last_rebooti
         LVM Status: $lvm_status
         Active Connections: $active_connections
         Number of Users: $user_count
-        IPv4 Address: $ipv4_info
-        MAC info: $mac_info
-        Sudo Commands Executed: $sudo_command_count"
+        IPv4 Address: $ipv4
+        MAC info: $mac
+        Sudo Commands Executed: $sudo"
