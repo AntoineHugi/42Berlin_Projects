57c217058eb129af50028bbd0cb005a0cd1303b7
AHugi <antoine.hugi@gmail.com>
2025-06-10 11:15:19 +0200

    exercise 00 cpp02

--- C++_modules/cpp02/ex00/Fixed.cpp
+++ C++_modules/cpp02/ex00/Fixed.cpp
@@ -2,23 +2,42 @@
 
 Fixed::Fixed():_fpnum(0)
 {
+	std::cout << "Default constuctor called" << std::endl;
 	return ;
 }
 
-Fixed::Fixed(const Fixed& other):_fpnum(other._fpnum)
+Fixed::Fixed(const Fixed& other)
 {
+	std::cout << "Copy constructor called" << std::endl;
+	this->operator=(other);
 	return ;
 }
 
 Fixed& Fixed::operator=(const Fixed& other)
 {
+	std::cout << "Copy assignment operator called" << std::endl;
 	if (this != &other)
 	{
-		this->_fpnum = other._fpnum;
+		this->_fpnum = other.getRawBits();
 	}
+	return (*this);
 }
 
 Fixed::~Fixed()
 {
+	std::cout << "Destructor called" << std::endl;
 	return ;
+}
+
+int	Fixed::getRawBits(void) const
+{
+	std::cout << "getRawBits member function called" << std::endl;
+	return (this->_fpnum);
+}
+
+void	Fixed::setRawBits(const int raw)
+{
+	std::cout << "setRawBits member function called" << std::endl;
+	this->_fpnum = raw;
+	return ; 
 }
